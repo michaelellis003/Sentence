4dd3eb35b7d5335f861823360184681188ae49ee
Michael Ellis <michaelellis003@gmail.com>
2025-03-09 17:35:49 -0500

    fix formatting

--- tests/test_sentence.py
+++ tests/test_sentence.py
@@ -5,7 +5,7 @@ from sentence import SentenceV1
 
 def test_sentence_v1():
     """Test the sentence_v1 function."""
-    s = SentenceV1('"The time has come," the Walrus said,')
+    s = SentenceV1('"The time has come," the Walrus said.')
 
     assert s.words == ['The', 'time', 'has', 'come', 'the', 'Walrus', 'said']
     assert s[0] == 'The'
